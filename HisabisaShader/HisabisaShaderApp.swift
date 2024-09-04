2f53a99e295c144d7acaace5f66b688f279037f0
HikaruHonda <hikaru.h.developer@gmail.com>
2024-09-04 18:36:29 +0900

    Initial Commit

---
+++ HisabisaShader/HisabisaShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HisabisaShaderApp.swift
+//  HisabisaShader
+//
+//  Created by 本田輝 on 2024/09/04.
+//
+
+import SwiftUI
+
+@main
+struct HisabisaShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
